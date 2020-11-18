a00c2351f9afd2c478353c2c33804e30462e108d
DavideMichielin <davidemichie@gmail.com>
2020-11-18 09:24:54 +0100

    We now convert 24 to Fizz

--- src/test/java/it/units/esSDM/Convert.java
+++ src/test/java/it/units/esSDM/Convert.java
@@ -28,4 +28,10 @@ public class Convert {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("Fizz", fizzBuzz.convert(6));
     }
+
+    @Test
+    void number_24_to_Fizz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.convert(24));
+    }
 }
